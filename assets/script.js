e8b32a329c961f265b5e35430d9f360174a0df5a
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-09 16:25:02 +0200

    add navbar

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,14 @@
-async function getGames() {
+// var navLinks = document.querySelectorAll(".nav-link");
+// navLinks.forEach(link => {
+//     link.addEventListener("click", function(){
+//         var category = link.getAttribute("data-category");
+//         console.log(category);
+//         getGames(category)
+//     })
+    
+// })
+
+async function getGames(category) {
     const options = {
         method: 'GET',
         headers: {
@@ -21,4 +31,34 @@ async function getGames() {
     // }
 }
 
-getGames()
+
+
+// var colShow = document.getElementById("colShow");
+//     arr.forEach(gaeDiv=>{
+//         var colGame = document.createElement("div");
+//         colGame.innerHTML = 
+//         `
+//             <div data-id=${gaeDiv.id} class="card h-100 bg-transparent" role="button">
+//                 <div class=" card-body">
+//                     <figure class="position-relative">
+//                         <img class="card-img-top object-fit-cover h-100"
+//                             src="https://www.freetogame.com/g/572/thumbnail.jpg">
+//                     </figure>
+//                     <figcaption>
+//                         <div class="hstack justify-content-between">
+//                             <h3 class="h6 small">Ravendawn</h3>
+//                             <span class="badge text-bg-primary p-2">Free</span>
+//                         </div>
+//                         <p class="card-text small text-center opacity-50">
+//                             A,2D,top-down,MMORPG,featuring,homesteading,and,building
+//                         </p>
+//                     </figcaption>
+//                 </div>
+//                 <footer class="card-footer small hstack justify-content-between">
+//                     <span class="badge badge-color">MMORPG</span>
+//                     <span class="badge badge-color">PC (Windows)</span>
+//                 </footer>
+//             </div>
+//         `
+//         colShow.appendChild(colGame);
+//     })
