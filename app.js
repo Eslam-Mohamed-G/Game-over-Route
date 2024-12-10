10a6071921205aaf59682e5ea5f70a0676d7bf12
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-10 20:38:36 +0200

    add forEach to navlinks so when click on it add active class and get category value

--- app.js
+++ app.js
@@ -15,4 +15,9 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
+    navLinks.forEach((link)=>{
+        link.addEventListener("click", () => {
+            navLinks.forEach( activeLink => { activeLink.classList.remove("active")})
+        })
+    });
 })
