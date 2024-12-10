ba8b54c0e5b8ef3d6a446129cd1f85d982a52bcb
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-10 19:19:13 +0200

    end of js and i try to do it by oop

--- assets/script.js
+++ assets/script.js
@@ -42,22 +42,7 @@ async function getGames(category) {
         console.error(error);
     }
 }
-// const url = 'https://free-to-play-games-database.p.rapidapi.com/api/game?id=452';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'x-rapidapi-key': '6a1ac68fc8msh7784b7711a286d5p197782jsn8fa5fa9c631a',
-// 		'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
-// 	}
-// };
 
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
 var colShow = document.getElementById("gameData");
 
 function displayDiv(games){
