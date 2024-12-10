372c58ef7b5a98c67aa503a997c2783f459b98cb
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-10 20:01:37 +0200

    add class GamesAPI in js by oop

---
+++ assets/api.js
@@ -0,0 +1,14 @@
+
+
+class GamesAPI {
+
+    constructor(){
+        this.options = {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-key': '6a1ac68fc8msh7784b7711a286d5p197782jsn8fa5fa9c631a',
+                'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
+            }
+        };
+    }
+}
