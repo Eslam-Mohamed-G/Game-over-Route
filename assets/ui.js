53b6b3e57f5dcc84ff4fcb53ee60f5ab74a430a2
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-10 20:16:35 +0200

    add clearContainer fun to clear container after click on category

--- assets/ui.js
+++ assets/ui.js
@@ -2,5 +2,9 @@ class UI{
     constructor(gameData) {
         this.container = document.getElementById("gameData");
     }
+
+    clearContainer(){
+        this.container.innerHTML = "";
+    }
 }
 export default UI
