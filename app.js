64e80eed4b1c7f1f015cbd3f44ad6d09da735712
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-10 20:24:46 +0200

    add the variables to handel it

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 import GamesAPI from './assets/api.js';
 import UI from './assets/ui.js';
 
+document.addEventListener("DOMContentLoaded", () => {
+    var api = new GamesAPI();
+    var ui = new UI("gameData");
+    var navLinks = document.querySelectorAll(".nav-link");
+    var toggler = document.querySelector(".navbar-toggler");
+    var menu = document.querySelector("#navbarSupportedContent");
+})
