8b80aca49eac9fb728379979f46b1e74453714e4
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-08 10:38:14 +0200

    add GamesDetails

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,9 +4,13 @@ import { createBrowserRouter } from 'react-router';
 import { RouterProvider } from 'react-router-dom';
 import Games from './Components/2_games/Games';
 import Layout from './Components/layout/Layout';
+import GamesDetails from './Components/3_gamesDetails/GamesDetails';
+import Navbar from './Components/1_header/Navbar';
 let routs = createBrowserRouter([
   {path:'', element : <Layout/>, children:[
-    // {index:true, element : <Games/>},
+    {index:true, element : <Navbar />},
+    {path:'games', element: <Games/>},
+    {path:'game/:id', element: <GamesDetails/>}
   ]}
 ])
 function App() {
