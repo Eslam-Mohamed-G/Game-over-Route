484aef81ab94e4f8cc61a123a7c579b173e37246
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-12 00:16:17 +0200

    add Footer component

--- React/src/Components/footer/Footer.jsx
+++ React/src/Components/footer/Footer.jsx
@@ -4,9 +4,10 @@ import logo from '../../assets/logo-sm.png';
 function Footer() {
     return (
         <div className='bg-black h-100 mt-5 d-flex'>
-            <div className="container d-flex">
-                <h1>footer</h1>
-                <img src={logo} alt="logo" style={{ maxWidth: 30,  maxHeight:30}} />
+            <div className="container d-flex py-2">
+                <div className="row">
+                    <h1>footer<img src={logo} alt="logo" style={{ maxWidth: 40 }} /></h1>
+                </div>
             </div>
         </div>
     )
